44d99578d9d6eadc7a6e0a84ed65666dca8a18ae
amt-merlo <amt.merlo@gmail.com>
2021-04-08 19:06:27 -0600

    Foreign Keys agregadas

--- creacionTablas.sql
+++ creacionTablas.sql
@@ -1,3 +1,15 @@
+
+DROP TABLE LB.Person_Type;
+DROP TABLE LB.Control;
+DROP TABLE LB.Book;
+DROP TABLE LB.Book_Clasification;
+DROP TABLE LB.Videogame;
+DROP TABLE LB.Movie;
+DROP TABLE LB.Magazine;
+DROP TABLE LB.Genre;
+DROP TABLE LB.Item;
+DROP TABLE LB.Person;
+
 --Se crea un esquema
 CREATE USER LB IDENTIFIED BY LB;
 
@@ -11,6 +23,8 @@ CREATE TABLE LB.Person_Type (ID INT PRIMARY KEY NOT NULL,
                              Type nchar(30) NOT NULL);
 
 CREATE TABLE LB.Control (ID INT PRIMARY KEY NOT NULL, 
+                         ID_Person INT NOT NULL,
+                         ID_Item INT NOT NULL,
                          Loan_Date DATE NOT NULL, 
                          Return_Date DATE NOT NULL, 
                          Days_Amount INT NOT NULL);
@@ -20,7 +34,9 @@ CREATE TABLE LB.Item (ID INT PRIMARY KEY NOT NULL,
                       Description NCHAR(60) NOT NULL, 
                       on_loan NUMBER(1,0));
 
-CREATE TABLE LB.Book (ID INT PRIMARY KEY NOT NULL, 
+CREATE TABLE LB.Book (ID INT PRIMARY KEY NOT NULL,
+                      ID_Clasification INT NOT NULL,
+                      ID_Item INT NOT NULL,
                       Title NCHAR(30) NOT NULL, 
                       Author NCHAR(30) NOT NULL, 
                       EDITION INT NOT NULL, 
@@ -32,17 +48,25 @@ CREATE TABLE LB.Book_Clasification (ID INT PRIMARY KEY NOT NULL,
                                     Clasification NCHAR(30) NOT NULL);
                                     
 CREATE TABLE LB.Videogame (ID INT PRIMARY KEY NOT NULL, 
+                           ID_Item INT NOT NULL,
                            Name NCHAR(30) NOT NULL, 
                            Developer NCHAR(30) NOT NULL,
                            Genre NCHAR(30) NOT NULL);
                            
 CREATE TABLE LB.Movie (ID INT PRIMARY KEY NOT NULL, 
+                       ID_Genre INT NOT NULL,
+                       ID_Item INT NOT NULL,
                        Title NCHAR(30) NOT NULL, 
                        MinDuration INT NOT NULL, 
                        Director NCHAR(30) NOT NULL, 
                        Score INT NOT NULL);
+                       
+CREATE TABLE LB.Genre (ID INT PRIMARY KEY NOT NULL,
+                       Genre NCHAR(30) NOT NULL);
+
                            
 CREATE TABLE LB.Magazine(ID INT PRIMARY KEY NOT NULL,
+                         ID_Item INT NOT NULL,
                          Title NCHAR(30) NOT NULL, 
                          Volume INT NOT NULL);                    
                            
@@ -50,6 +74,36 @@ CREATE TABLE LB.Magazine(ID INT PRIMARY KEY NOT NULL,
                            
                            
                            
-                           
-                           
+ALTER TABLE LB.Control
+ADD CONSTRAINT FK_Person
+FOREIGN KEY (ID_Person) REFERENCES LB.Person(ID_Number);
+
+ALTER TABLE LB.Control
+ADD CONSTRAINT FK_ControlItem
+FOREIGN KEY (ID_Item) REFERENCES LB.Item(ID);
+
+            
+ALTER TABLE LB.Book
+ADD CONSTRAINT FK_Clasification
+FOREIGN KEY (ID_Clasification) REFERENCES LB.Book_Clasification(ID);      
+
+ALTER TABLE LB.Book
+ADD CONSTRAINT FK_BookItem
+FOREIGN KEY (ID_Item) REFERENCES LB.Item(ID);
+
+ALTER TABLE LB.Videogame
+ADD CONSTRAINT FK_GameItem
+FOREIGN KEY (ID_Item) REFERENCES LB.Item(ID);
+
+ALTER TABLE LB.Movie
+ADD CONSTRAINT FK_Genre
+FOREIGN KEY (ID_Genre) REFERENCES LB.Genre(ID);
+
+ALTER TABLE LB.Movie
+ADD CONSTRAINT FK_MovieItem
+FOREIGN KEY (ID_Item) REFERENCES LB.Item(ID);
+
+ALTER TABLE LB.Magazine
+ADD CONSTRAINT FK_MagazineItem
+FOREIGN KEY (ID_Item) REFERENCES LB.Item(ID);
                            
